4cf83acacb2df81f462ac25203aa86c3db05d815
Martyn Janes <martyn@obany.com>
2024-06-17 09:34:40 +0200

    Add propertyDelete

--- packages/crypto-cli/src/cli.ts
+++ packages/crypto-cli/src/cli.ts
@@ -22,7 +22,7 @@ export class CLI extends CLIBase {
 			{
 				title: "GTSC Crypto",
 				appName: "gtsc-crypto",
-				version: "0.0.4-next.29",
+				version: "0.0.4-next.30",
 				icon: "🌍",
 				supportsEnvFiles: true
 			},
--- packages/crypto-cli/tests/cli.spec.ts
+++ packages/crypto-cli/tests/cli.spec.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0.
 import path from "node:path";
 import { CLIDisplay } from "@gtsc/cli-core";
-import chalk from "chalk";
 import { CLI } from "../src/cli";
 
 let writeBuffer: string[] = [];
@@ -28,7 +27,7 @@ describe("CLI", () => {
 		const exitCode = await cli.run(["", path.join(__dirname, "crypto-cli")], localesDirectory);
 		expect(exitCode).toBe(0);
 		expect(writeBuffer.length).toEqual(17);
-		expect(writeBuffer[0]).toEqual(`🌍 ${chalk.underline.bold.blue("GTSC Crypto v0.0.4-next.29")}`);
+		expect(writeBuffer[0].includes("GTSC Crypto v0.0.4-next.30")).toEqual(true);
 		expect(writeBuffer[1]).toEqual("");
 		expect(writeBuffer[2]).toEqual("");
 		expect(writeBuffer[3]).toEqual("");
