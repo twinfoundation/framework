6a909193f7fa13c764746d9c85f1e5cd28ee010c
Jose Manuel Cantera <jose.cantera@iota.org>
2024-07-26 16:42:36 +0200

    Property formats

--- packages/entity/src/models/entitySchemaPropertyFormat.ts
+++ packages/entity/src/models/entitySchemaPropertyFormat.ts
@@ -19,7 +19,67 @@ export const EntitySchemaPropertyFormat = {
 	/**
 	 * email.
 	 */
-	Email: "email"
+	Email: "email",
+
+	/**
+	 * int8.
+	 */
+	Int8: "int8",
+
+	/**
+	 * uint8.
+	 */
+	Uint8: "uint8",
+
+	/**
+	 * int16.
+	 */
+	Int16: "int16",
+
+	/**
+	 * uint16.
+	 */
+	Uint16: "uint16",
+
+	/**
+	 * int32.
+	 */
+	Int32: "int32",
+
+	/**
+	 * uint32.
+	 */
+	Uint32: "uint32",
+
+	/**
+	 * float.
+	 */
+	Float: "float",
+
+	/**
+	 * double.
+	 */
+	Double: "double",
+
+	/**
+	 * int64.
+	 */
+	Int64: "int64",
+
+	/**
+	 * uint64.
+	 */
+	Uint64: "uint64",
+
+	/**
+	 * date.
+	 */
+	Date: "date",
+
+	/**
+	 * date-time.
+	 */
+	DateTime: "date-time"
 
 } as const;
 
