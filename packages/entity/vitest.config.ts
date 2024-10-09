9d408636cfa36a40de3f8f5ff7acf3f5a4ab4ec4
Martyn Janes <martyn@obany.com>
2024-10-09 11:20:48 +0200

    Update test config

--- packages/entity/vitest.config.ts
+++ packages/entity/vitest.config.ts
@@ -1,6 +1,7 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
 import typescript from "rollup-plugin-typescript2";
+import tspc from "ts-patch/compiler";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
 	plugins: [
 		typescript({
 			tsconfig: "./tests/tsconfig.json",
+			typescript: tspc,
 			clean: true
 		})
 	],
@@ -15,8 +17,8 @@ export default defineConfig({
 		include: ["./tests/**/*.spec.ts"],
 		globals: true,
 		bail: 1,
-		testTimeout: 120000,
-		hookTimeout: 120000,
+		testTimeout: 300000,
+		hookTimeout: 300000,
 		coverage: {
 			provider: "v8",
 			reporter: ["text", "lcov"],
