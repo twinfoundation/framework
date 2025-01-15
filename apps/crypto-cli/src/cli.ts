ccf19d9f12186ee46ee6b57b560cd134f0b9e522
Martyn Janes <martyn@obany.com>
2025-01-15 14:22:52 +0200

    Update crypto packages

--- apps/crypto-cli/src/cli.ts
+++ apps/crypto-cli/src/cli.ts
@@ -28,7 +28,7 @@ export class CLI extends CLIBase {
 			{
 				title: "TWIN Crypto",
 				appName: "twin-crypto",
-				version: "0.0.1-next.19",
+				version: "0.0.1-next.20",
 				icon: "🌍",
 				supportsEnvFiles: true,
 				overrideOutputWidth: options?.overrideOutputWidth
--- packages/crypto/docs/changelog.md
+++ packages/crypto/docs/changelog.md
@@ -1,5 +1,5 @@
 # @twin.org/crypto - Changelog
 
-## 0.0.1-next.19
+## 0.0.1-next.20
 
 - Added: Bip44
