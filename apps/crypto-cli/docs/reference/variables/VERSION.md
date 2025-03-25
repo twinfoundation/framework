ba7d2fef90fc41218e1505af01db34583166442d
Martyn Janes <martyn@obany.com>
2025-03-25 10:00:08 +0200

    chore: add extra-files

---
+++ apps/crypto-cli/docs/reference/variables/VERSION.md
@@ -0,0 +1,3 @@
+# Variable: VERSION
+
+> `const` **VERSION**: `"0.0.1-next.49"` = `"0.0.1-next.49"`
---
+++ apps/crypto-cli/src/version.ts
@@ -0,0 +1,3 @@
+// Copyright 2024 IOTA Stiftung.
+// SPDX-License-Identifier: Apache-2.0.
+export const VERSION = "0.0.1-next.49"; // x-release-please-version
