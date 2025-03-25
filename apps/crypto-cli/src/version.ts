2893e75ae5dfe97cf9350af7e56891d4c01f4b55
Martyn Janes <martyn@obany.com>
2025-03-25 12:08:47 +0200

    chore: release next

--- apps/crypto-cli/src/version.ts
+++ apps/crypto-cli/src/version.ts
@@ -1,3 +1,3 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-export const VERSION = "0.0.1-next.49"; // x-release-please-version
+export const VERSION = "1.0.0"; // x-release-please-version
