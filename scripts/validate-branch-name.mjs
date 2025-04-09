4baa1015acb09968debac6c19ae7da0e499af1ef
Martyn Janes <martyn@obany.com>
2025-04-09 10:46:20 +0200

    chore: add pr name checker

--- scripts/validate-branch-name.mjs
+++ scripts/validate-branch-name.mjs
@@ -3,11 +3,13 @@
 import { execSync } from 'node:child_process';
 
 const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-const pattern = /^(feature|fix|release|chore)\/[\da-z-]+?$/i;
+const pattern = /^(feature|bugfix|hotfix|release|chore)\/[\da-z-]+?$/;
 
 if (!pattern.test(branchName) && branchName !== 'next') {
 	process.stderr.write(`ERROR: Branch name '${branchName}' doesn't match the required pattern.\n`);
-	process.stderr.write('Branch names should start: feature/, fix/, release/ or chore/\n');
+	process.stderr.write(
+		'Branch names should start: feature/, bugfix/, hotfix/, release/ or chore/\n'
+	);
 	process.stderr.write('and the name should consist of lowercase letters, numbers and hyphens.\n');
 	// eslint-disable-next-line unicorn/no-process-exit
 	process.exit(1);
